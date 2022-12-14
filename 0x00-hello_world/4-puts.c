eb081b09a29a6b610cebd28c2888ac3540f05f55
Afeezope <afeezope1994@gmail.com>
2022-12-14 15:01:57 -0800

    new-file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - The program prints "Programming is like a multilingual puzzle"
+ * Return: 0 if executed successfully, non-zero otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
