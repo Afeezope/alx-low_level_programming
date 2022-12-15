3c8ee0a16ae3b32f7e39f65b939136b78b3f45ca
Afeezope <afeezope1994@gmail.com>
2022-12-15 00:52:50 -0800

    new-file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly with proper grammar, 
- * but the outcome is a piece of art
+ * main - prints with proper grammar, but the outcome is a piece of art
  * return: 0 if executed successfully, non-zero otherwise
  */
 
-
 int main(void)
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
