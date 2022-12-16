02569a21a9e2b8513dc1b131322bd232d5aa2233
Afeezope <afeezope1994@gmail.com>
2022-12-16 03:57:37 -0800

    new-file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,8 @@
 #include <unistd.h>
 
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * main - prints "and that piece of art is useful" 
+ * - Dora Korpar, 2015-10-19"
  * followed by a new line, to the standard error.
  * Return: 0 for success
  */
