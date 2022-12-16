dcfc8155c310431114ee05b38a7d399063fe8add
Afeezope <afeezope1994@gmail.com>
2022-12-16 00:19:10 -0800

    space-removed-from-code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints with proper grammar, but the outcome is a piece of art
- * return: 0 if executed successfully, non-zero otherwise
+ * main - prints with proper grammar
+ * but the outcome is a piece of art
+ * return: 0 if executed sucessfully, non-zero otherwise
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
