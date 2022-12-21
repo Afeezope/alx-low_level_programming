54cfc710e27a3fe8d120c98023800d564b5c27ed
Afeezope <afeezope1994@gmail.com>
2022-12-20 23:45:10 -0800

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all lowercase letters except q and e
+ * Return: 0 if executed successfully
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
