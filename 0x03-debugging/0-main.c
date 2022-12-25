e0518d219686884c68c4f5663920ebe64bc12d16
Afeezope <afeezope1994@gmail.com>
2022-12-25 05:13:22 -0800

    new-files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints whether integer is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 98 % 2;
+	positive_or_negative(n);
+
+	return (0);
+}
