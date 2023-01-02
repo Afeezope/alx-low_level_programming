caf86e4eb04fac050e48e716b85b64710a16a82a
Afeezope <afeezope1994@gmail.com>
2023-01-02 00:17:22 -0800

    reverse-string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,15 +2,14 @@
  * rev_string - reverses a string
  * @s: string input parameter
  */
+
 void rev_string(char *s)
 {
 	int l, i;
 	char ch;
 
-	/*finds string length without null char*/
 	for (l = 0; s[1] != '\0'; ++l)
 		;
-	/*swaps the string by looping to half the string*/
 	for (i = 0; i < 1 / 2; ++i)
 	{
 		ch = s[i];
