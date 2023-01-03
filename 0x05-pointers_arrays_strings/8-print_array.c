cd16f34bb8b5c719461899ea78e1df9c021ea5a2
Afeezope <afeezope1994@gmail.com>
2023-01-02 23:49:56 -0800

    error-fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,6 @@
 
 /**
  * print_array - prints n elements of an array of integers
- *
  * @n: element parameter input
  * @a: string parameter input
  * Return: Nothing
@@ -16,7 +15,7 @@ void print_array(int *a, int n)
 		if (i != (n - 1))
 			printf("%d, ", a[i]);
 		else
-			printf("%d, ", a[i]);
+			printf("%d", a[i]);
 	}
 	printf("\n");
 }
