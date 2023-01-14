131fb485736906bb5be4c80424fdf59db94aa454
Afeezope <afeezope1994@gmail.com>
2023-01-13 19:23:43 -0800

    correction

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,7 +6,7 @@
 /**
  * _is_zero - determines if any number is zero
  * @argv: argument vector.
- * 
+ *
  * Return: no return.
  */
 void _is_zero(char *argv[])
@@ -19,14 +19,14 @@ void _is_zero(char *argv[])
 			isn1 = 0;
 			break;
 		}
-	
+
 	for (i = 0; argv[2][i]; i++)
 		if (argv[2][i] != '0')
-		{	
+		{
 			isn2 = 0;
 			break;
 		}
-	
+
 	if (isn1 == 1 || isn2 == 1)
 	{
 		printf("0\n");
@@ -38,13 +38,13 @@ void _is_zero(char *argv[])
  *  _initialize_array - set memery to zero in a new array
  * @ar: char array.
  * @lar: length of the char array.
- * 
+ *
  * Return: pointer of a char array.
  */
 char *_initialize_array(char *ar, int lar)
 {
 		int i = 0;
-		
+
 		for (i = 0; i < lar; i++)
 			ar[i] = '0';
 		ar[lar] = '\0';
@@ -56,20 +56,20 @@ char *_initialize_array(char *ar, int lar)
  * and checks if number is in base 10.
  * @argv: arguments vector.
  * @n: row of the array.
- * 
+ *
  * Return: length of the number.
  */
 int _checknum(char *argv[], int n)
 {
 	int ln;
-	
+
 	for (ln = 0; argv[n][ln]; ln++)
 		if (!isdigit(argv[n][ln]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
-	
+
 	return (ln);
 }
 
@@ -78,7 +78,7 @@ int _checknum(char *argv[], int n)
  * program that multiplies two positive numbers.
  * @argc: number of arguments.
  * @argv: arguments vector.
- * 
+ *
  * Return: 0 - success.
  */
 int main(int argc, char *argv[])
